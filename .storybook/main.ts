a28791ab88149d41afbe713659a03cdac05c6b13
Greg Hovanesyan <greg.hovanesyan@gmail.com>
2023-05-09 19:07:41 +0200

    Setup Storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,27 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
+
+const config: StorybookConfig = {
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)", "../components/**/*.stories.@(js|jsx|ts|tsx)", "../components/**/*.stories.mdx"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  webpackFinal: async (config, { }) => {
+    config.resolve.alias = {
+      ...config.resolve?.alias,
+      '@': path.resolve(__dirname, "..")
+    };
+
+    return config;
+  },
+};
+export default config;
